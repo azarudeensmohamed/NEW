18160eb491f94061ec9499ee8f71db38aa0f5fc7
azarudeensmohamed <mohamedazar201199@gmail.com>
2025-09-02 12:23:29 -0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+   domains: ['drahomes.in', 'slelguoygbfzlpylpxfs.supabase.co','websitedemos.net','localhost:300', 'radianrise.netlify.app'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+
+export default nextConfig;
